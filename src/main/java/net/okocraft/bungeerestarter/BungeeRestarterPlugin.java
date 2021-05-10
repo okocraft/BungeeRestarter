e378bdca307b4472a117aa49b2e67b0b17ad60cd
Siroshun09 <siro.shun8@gmail.com>
2021-05-10 13:20:07 +0900

    feat(plugin): add source codes

---
+++ src/main/java/net/okocraft/bungeerestarter/BungeeRestarterPlugin.java
@@ -0,0 +1,48 @@
+package net.okocraft.bungeerestarter;
+
+import com.github.siroshun09.configapi.common.util.ResourceUtils;
+import com.github.siroshun09.configapi.yaml.YamlConfiguration;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+
+public class BungeeRestarterPlugin extends Plugin {
+
+    private final YamlConfiguration config = YamlConfiguration.create(getDataFolder().toPath().resolve("config.yml"));
+
+    @Override
+    public void onEnable() {
+        try {
+            Files.createDirectories(config.getPath().getParent());
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Could not create plugin directory.", e);
+        }
+
+        try {
+            ResourceUtils.copyFromClassLoaderIfNotExists(getClass().getClassLoader(), "config.yml", config.getPath());
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Could not save default config.yml", e);
+            return;
+        }
+
+        try {
+            config.load();
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Could not load config.yml", e);
+            return;
+        }
+
+        getProxy().getPluginManager().registerCommand(this, new RestartCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getProxy().getPluginManager().unregisterCommands(this);
+    }
+
+    public YamlConfiguration getConfig() {
+        return config;
+    }
+}
---
+++ src/main/java/net/okocraft/bungeerestarter/RestartCommand.java
@@ -0,0 +1,72 @@
+package net.okocraft.bungeerestarter;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+public class RestartCommand extends Command {
+
+    private final BungeeRestarterPlugin plugin;
+
+    public RestartCommand(BungeeRestarterPlugin plugin) {
+        super("bungeerestart", null, "brestart");
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+        if (!commandSender.hasPermission("bungeerestarter.command")) {
+            commandSender.sendMessage(TextComponent.fromLegacyText("You don't have the permission to restart proxy."));
+            return;
+        }
+
+        var command = plugin.getConfig().getString("command-to-start-proxy");
+
+        if (command.isEmpty()) {
+            commandSender.sendMessage(
+                    TextComponent.fromLegacyText("The command to start proxy is not set in config.yml")
+            );
+            return;
+        }
+
+        plugin.getProxy().getPlayers().forEach(this::kickPlayer);
+
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException ignored) {
+        }
+
+        var shutdownHook =
+                new Thread(() -> {
+                    try {
+                        Runtime.getRuntime().exec(command);
+                    } catch (IOException e) {
+                        plugin.getProxy().getLogger().log(
+                                Level.SEVERE,
+                                "Could not execute command \"" + command + "\"",
+                                e
+                        );
+                    }
+                });
+
+        shutdownHook.setDaemon(true);
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+        plugin.getProxy().stop();
+    }
+
+    private void kickPlayer(ProxiedPlayer player) {
+        var kickMessageKey = "kick-message." + player.getLocale().toString();
+        var kickMessage = plugin.getConfig().getString(kickMessageKey);
+
+        if (kickMessage.isEmpty()) {
+            kickMessage = plugin.getConfig().getString("kick-message.default");
+        }
+
+        player.disconnect(TextComponent.fromLegacyText(kickMessage));
+    }
+}
